e292dc8518992d0d642eb945f24f567493b92d01
vboxuser <vboxuser@WINDOWS-11>
2023-10-02 14:08:30 +0300

    Add project files.

---
+++ Qrcodegenerator/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using QRCoder;
+
+namespace Qrcodegenerator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string text=textBox1.Text; 
+
+            QRCodeGenerator codeGenerator = new QRCodeGenerator();
+            QRCodeData qRCodeData = codeGenerator.CreateQrCode(text, QRCodeGenerator.ECCLevel.Q);
+            QRCode qrCode = new QRCode(qRCodeData);
+            Bitmap bmp = qrCode.GetGraphic(20);
+            pictureBox1.Image= bmp;
+        }
+
+        private void textBox1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+        }
+    }
+}
